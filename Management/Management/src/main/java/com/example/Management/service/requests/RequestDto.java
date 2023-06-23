1c82f75fc6580a972f0e380a99495833e4988194
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-06-23 23:15:17 +0300

    Updated Register requests

--- Management/Management/src/main/java/com/example/Management/service/requests/RequestDto.java
+++ Management/Management/src/main/java/com/example/Management/service/requests/RequestDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class RequestDto {
 
+	private String firstName;
+	private String lastName;
 	private String username;
 	private String password;
 	private String role;
