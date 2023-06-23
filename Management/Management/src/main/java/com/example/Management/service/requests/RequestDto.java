45d91b4de51aa56627c8dec2b6d0abadc4db1d41
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-06-23 23:04:39 +0300

    Added requests and responses

---
+++ Management/Management/src/main/java/com/example/Management/service/requests/RequestDto.java
@@ -0,0 +1,11 @@
+package com.example.Management.service.requests;
+
+import lombok.Data;
+
+@Data
+public class RequestDto {
+
+	private String username;
+	private String password;
+	private String role;
+}
---
+++ Management/Management/src/main/java/com/example/Management/service/responses/AuthResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.Management.service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDto {
+
+	private String accessToken;
+	private String tokenType = "Bearer ";
+	
+	public AuthResponseDto (String accessToken) {
+		this.accessToken=accessToken;
+	}
+}
