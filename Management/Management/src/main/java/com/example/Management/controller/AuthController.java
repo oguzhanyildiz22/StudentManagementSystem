06e95b3edae35b8b7631ba21b046623997b51323
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-06-23 23:04:20 +0300

    Added Controller class to register and login

---
+++ Management/Management/src/main/java/com/example/Management/controller/AuthController.java
@@ -0,0 +1,55 @@
+package com.example.Management.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Management.service.requests.LoginDto;
+import com.example.Management.service.requests.RequestDto;
+import com.example.Management.service.responses.AuthResponseDto;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+	@Autowired
+	private AuthService authService;
+
+	
+
+	@PostMapping("login")
+	public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto) {
+
+		
+		AuthResponseDto authResponseDto = authService.login(loginDto);
+		
+		return new ResponseEntity<>(authResponseDto, HttpStatus.OK);
+	}
+
+	@PostMapping("register")
+	public String register(@RequestBody RequestDto requestDto) {
+		
+		String result = authService.register(requestDto);
+		return result;
+
+	}
+	
+
+	@GetMapping("/getRole")
+	public List<SimpleGrantedAuthority> getRole(@RequestHeader("Authorization") String authorizationHeader) {
+
+		return authService.getRole(authorizationHeader);
+	}
+
+}
