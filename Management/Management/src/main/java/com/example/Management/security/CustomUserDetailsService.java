f031b35078a998b2cf9bf3b036f891ae9b782c33
Oguzhan Yıldız <oguzy2020@gmail.com>
2023-06-23 20:57:49 +0300

    Created CustomUserDetailsService to pull User info From database

---
+++ Management/Management/src/main/java/com/example/Management/security/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.example.Management.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.Management.entity.Role;
+import com.example.Management.entity.UserEntity;
+import com.example.Management.repository.UserRepository;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+	    return new User(user.getUsername() , user.getPassword() , mapRolesAuthorities(user.getRoles()));
+	}
+	/**
+	 * Converts the given list of roles to a collection of SimpleGrantedAuthority objects.
+	 * @param roles The list of roles to be converted.
+	 * @return A collection of SimpleGrantedAuthority objects. Returns null if the role list is empty.
+	 */
+	private Collection<GrantedAuthority> mapRolesAuthorities(List<Role> roles){
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+	}
+	
+
+}
